89fbe92d6516c242f87457d00ef3e2cedd3d4862
I-Unknown-I <62710051+I-Unknown-I@users.noreply.github.com>
2024-10-03 12:19:17 +0300

    Add files via upload

---
+++ src/task4.java
@@ -0,0 +1,37 @@
+public class task4
+{
+    public static void main(String[] args)
+    {
+        final int n = 5;// размер
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n;j++)
+            {
+                matrix[i][j] = (int) (Math.random() * (198)) - 99;
+
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n;j++)
+            {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        int min = 100;
+        for(int i =n-1;i>=0;)
+        {
+            for(int j =0;j<n;j++)
+            {
+            if(matrix[i][j]<min)
+                min=matrix[i][j];
+            i--;
+            }
+        }
+        System.out.println(min);
+
+
+    }
+}
