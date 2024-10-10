401c3f3e252cafd33811a5d0af4f9a45685c119e
Fear_Folamh <62710051+FearFolamh@users.noreply.github.com>
2024-10-10 14:22:57 +0300

    Add files via upload

--- src/task3.java
+++ src/task3.java
@@ -1,18 +1,31 @@
+import java.util.*;
+
 public class task3 {
     public static void main(String[] args){
 
         final int rows = 3;// кол строк
         final int cols = 3;// кол колон
         int[][] matrix = new int[rows][cols];
-        int[] b = new int [cols];
+        Vector b = new Vector() ;
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols;j++)
             {
                 matrix[i][j] = (int) (Math.random() * (198)) - 99;
-                if (matrix[i][j]>=0)
-                    b[i]++;
             }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            int k = 0;
+            for (int j = 0; j < cols;j++)
+            {
+                if (matrix[i][j]>=0)
+                    k++;
+
+            }
+            b.addElement(k);
+
         }
         System.out.println("Матрица");
 
@@ -25,9 +38,10 @@ public class task3 {
             System.out.println();
         }
         System.out.println("Вектор b");
-        for (int i = 0; i < rows; i++)
+        Enumeration BN = b.elements();
+        while(BN.hasMoreElements())
         {
-            System.out.println(b[i]);
+            System.out.println(BN.nextElement());
 
         }
     }
